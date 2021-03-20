73573ef5900235dbf3e390f25dfd354942a170cc
harsharay <harsharay.ray@gmail.com>
2021-03-21 01:04:33 +0530

    First working version

---
+++ src/Components/Home/Home.js
@@ -0,0 +1,94 @@
+import React, { useState, useEffect } from "react"
+import InfiniteScroll from "react-infinite-scroll-component";
+import Masonry from "react-masonry-css";
+import Modal from "../Modal/Modal"
+
+import { createApi } from "unsplash-js";
+
+import "./Home.css"
+
+
+const api = createApi({
+    // Don't forget to set your access token here!
+    // See https://unsplash.com/developers
+    accessKey: "BE1lkEqCStnQ146IpuaUabcy8OBPfCAL-8GMzUdJm2s"
+});
+
+const Home = () => {
+
+    const [results, setResults] = useState([])
+    const [loading, setLoading] = useState(false)
+    const [imageClicked, setImageClicked] = useState(false)
+    const [currentImage, setCurrentImage] = useState("")
+    const [currentIndex, setCurrentIndex] = useState(null)
+
+    useEffect(() => {
+        api.photos.getRandom({count: 15})
+        .then(data => {
+            setResults(data.response)
+            console.log(30,data.response)
+        })
+        // fetch("https://jsonplaceholder.typicode.com/photos")
+        // .then(data => data.json())
+        // .then(json => setResults(json))
+    }, []);
+
+    const fetchMoreData = () => {
+        if(!loading) {
+            setLoading(true)
+            api.photos.getRandom({count: 15})
+            .then(data => {
+                setLoading(false)
+                setResults(results.concat(data.response))
+                
+            })
+            // fetch("https://jsonplaceholder.typicode.com/photos")
+            // .then(data => data.json())
+            // .then(json => {
+            //     setLoading(false)
+            //     setResults(results.concat(json))
+            // })
+        }
+    }
+
+    const handleImageClick = index => {
+        setImageClicked(true)
+        console.log(55,results[index])
+        setCurrentImage(results[index])
+        setCurrentIndex(index)
+    }
+
+    return (
+        <div>
+            <div>
+                <InfiniteScroll 
+                    dataLength = {results.length}
+                    next = {fetchMoreData}
+                    hasMore = {true}
+                    loader = {<h4>Loading...</h4>}
+                    className="images-box"
+                >
+                    {/* <Masonry
+                        breakpointCols={3}
+                        className="my-masonry-grid"
+                        columnClassName="my-masonry-grid_column"> */}
+                            <div className="photos-group">
+                                {results.map((item, index) =>  {
+                                    return (
+                                        <div key={index} onClick={() => handleImageClick(index)}>
+                                            <img src={item.urls.regular} alt={item.alt_description} className="single-photo"/>
+                                            {/* <img src={item.url} alt={item.title} className="single-photo"/> */}
+                                        </div>
+                                    )
+                                }
+                                )}
+                            </div>
+                    {/* </Masonry> */}
+                </InfiniteScroll>
+                {imageClicked && <Modal results={results} currentImage={currentImage} currentIndex={currentIndex}/>}
+            </div>
+        </div>
+    )
+}
+
+export default Home;
---
+++ src/Components/Modal/Modal.js
@@ -0,0 +1,41 @@
+import React, { useEffect, useState } from "react"
+
+import "./Modal.css"
+
+
+const Modal = (props) => {
+
+    const [currentIndexModal, setCurrentIndexModal] = useState(props.currentIndex)
+    // const [currentImageToDisplay, setCurrentImageToDisplay] = useState(props.currentImage)
+
+    // useEffect(() => {
+    //     console.log(props)
+    // },[currentIndexModal])
+
+    const handleNavigation = e => {
+        const {name} = e.target
+
+        if(name === "left"){
+            setCurrentIndexModal(currentIndexModal-1)
+        } else {
+            setCurrentIndexModal(currentIndexModal+1)
+        }
+    }
+
+
+    return (
+        <div className="modal-root">
+            <div className="modal-content">
+                {/* {JSON.stringify(props.results[currentIndexModal])} */}
+                {/* <p>{props.results[currentIndexModal].alt_description}</p> */}
+                <img src={props.results[currentIndexModal].urls.regular} alt={props.results[currentIndexModal].alt_description}/>
+                <div className="modal-navigation">
+                    {currentIndexModal >0 && <button onClick={handleNavigation} name="left">Previous</button>}
+                    {currentIndexModal <= props.results.length && <button onClick={handleNavigation} name="right">Next</button>}
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Modal;
